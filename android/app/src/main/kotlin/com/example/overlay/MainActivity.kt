64e6bcc84ab59b08b10bd6ef9f8bd2592dcbf97c
Anthony(Bumkyoo Shim) <anthony@heybit.io>
2023-08-28 22:38:47 +0900

    feat: 오버레이 추가

--- android/app/src/main/kotlin/com/example/overlay/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/overlay/MainActivity.kt
@@ -1,6 +1,22 @@
 package com.example.overlay
 
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, ForegroundService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Log.d("krrr", "startForegroundService")
+            startForegroundService(intent)
+        } else {
+            Log.d("krrr", "startService")
+            startService(intent)
+        }
+    }
 }
